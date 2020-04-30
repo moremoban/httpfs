304964accc6a2b34cba00b78c5df1b7622b73d8c
chfw <wangc_2011@hotmail.com>
2020-04-30 23:58:33 +0100

    :sparkles: read-only http file system can work with moban with some constraints

---
+++ httpfs/fs.py
@@ -0,0 +1,95 @@
+import logging
+from io import BytesIO
+from unittest.mock import MagicMock
+
+import httpx
+from fs.base import FS
+from fs.errors import CreateFailed
+
+LOG = logging.getLogger(__name__)
+
+
+class HttpFs(FS):
+    def __init__(self, url):
+        self.base_url = url
+        self.cache = {}
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        pass
+
+    def getinfo(self, path, namespaces=None):
+        LOG.info("getinfo")
+        flag = "." not in path
+        return MagicMock(is_dir=flag)
+
+    def listdir(self, path):
+        return []
+
+    def makedir(self):
+        pass
+
+    def openbin(self, a_file, **_):
+        LOG.info("openbin")
+        try:
+            if self.base_url.endswith("/"):
+                url = self.base_url + a_file
+            else:
+                url = self.base_url + "/" + a_file
+                content = self.download_file(url)
+            return BytesIO(content)
+        except NoContentFound:
+            raise CreateFailed
+
+    def remove(self):
+        pass
+
+    def removedir(self):
+        pass
+
+    def setinfo(self):
+        pass
+
+    def exists(self, a_file):
+        LOG.info("exists")
+        if a_file.endswith("/"):
+            return True
+        try:
+            if self.base_url.endswith("/"):
+                url = self.base_url + a_file
+            else:
+                url = self.base_url + "/" + a_file
+            self.download_file(url)
+            return True
+        except NoContentFound:
+            return False
+
+    def download_file(self, url):
+        LOG.info("download_file")
+
+        if url in self.cache:
+            return self.cache[url]
+
+        r = httpx.get(url)
+
+        content = r.content
+
+        if r.status_code == 200:
+            self.cache[url] = content
+            return content
+        else:
+            LOG.info(f"status code: {r.status_code}")
+            raise NoContentFound()
+
+    def geturl(self, a_file, **_):
+        if self.base_url.endswith("/"):
+            url = self.base_url + a_file
+        else:
+            url = self.base_url + "/" + a_file
+        return url
+
+
+class NoContentFound(Exception):
+    pass
--- httpfs/opener.py
+++ httpfs/opener.py
@@ -1,22 +1,11 @@
-import httpx
 from fs.opener import Opener
-from fs.memoryfs import MemoryFS
 
-TEXT_FILE_TYPES = ["text/plain"]
+from httpfs.fs import HttpFs
 
 
 class HttpFsOpener(Opener):
     protocols = ["http", "https"]
 
     def open_fs(self, fs_url, parse_result, writeable, create, cwd):
-        r = httpx.get(fs_url)
-
-        content_type, _ = r.headers["content-type"].split(";")
-
-        file_name = parse_result.resource.split("/")[-1]
-        mem_fs = MemoryFS()
-        if content_type in TEXT_FILE_TYPES:
-            mem_fs.writetext(file_name, r.text)
-        else:
-            mem_fs.writebytes(file_name, r.content)
-        return mem_fs
+        http_fs = HttpFs(fs_url)
+        return http_fs
